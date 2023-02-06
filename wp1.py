33201f75c6a55f1578a99001b563c10b3c7cade4
Niko <niko.konzack@t-online.de>
2023-02-06 12:37:10 +0100

    added entry point to the script

--- wp1.py
+++ wp1.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import networkx as nx
 
 amino_acid_list = ["L-arginine", "L-valine", "L-methionine", "L-glutamate", "L-glutamine", "L-tyrosine", "L-tryptophan",
@@ -69,14 +69,62 @@ def bf_traversal(g: nx.DiGraph, metabolite: str) -> nx.DiGraph:
     """ 
     Takes a metabolites to form a subgraph constructed from them
     """
-    # Niko
+    
+    # Assume there is enough energy in the system
+
+    # Mark the starting node as visited
+    g.nodes[metabolite]["visited"] = True
+
+    # Define nodes to search
+    outgoing_edges: List[Tuple[str, str, bool]] = g.edges(metabolite).data("visited", default=False)
+    new_outgoing_edges: List[Tuple[str, str, bool]] = []
+
+    # Search every outgoing edge
+    while len(outgoing_edges) > 0:
+        for u, v, visited in outgoing_edges:
+
+            # Check if the node has all its required inputs
+
+            # Visit the node if it has not been visited yet
+            if not visited:
+                g.nodes[v][visited] = True
+
+                # Remember the neighbourhood of this node for the next iteration
+                for edge in g.edges(v).data("visited", default=False):
+                    new_outgoing_edges.append(edge) 
+
+        # Continue with the next iteration
+        outgoing_edges = new_outgoing_edges
+        new_outgoing_edges = []
+
+
+    # Mark it as visited
+    g.nodes[metabolite]["visited"] = True
+
+    # Iterate over the OUTGOING neighborhood of the starting element
+    for u,v in g.edges(metabolite):
+
+        # Mark every searched node as visited
+        g.nodes[v]["visited"] = True
+
+        # Continue search if all input nodes have already been visited
+
+
+
+
+    
+
     pass
 
-def reverse_bf_traversal():
-    pass
-    # Niko
+def reverse_bf_traversal(g: nx.DiGraph):
+    return bf_traversal(g.reverse())
 
 def intersect_subgraph(s: nx.DiGraph, subgraphs: List[nx.DiGraph]) -> nx.DiGraph:
     """ Intersection of the given subgraphs """
     # Alwine
     pass
+
+if __name__ == "__main__":
+    # 1. Done
+    # 2. Parse the file and construct the graph
+
