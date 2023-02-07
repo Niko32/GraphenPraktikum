dcd3e4a6ef295a2f79b91116f6f0a41fb84e9628
Alwine Balfanz <alwine@balfanz.eu>
2023-02-07 10:40:47 +0100

    init

---
+++ wp1_analyzer.py
@@ -0,0 +1,29 @@
+import networkx as nx
+
+def generate_subgraphs() -> dict[str, nx.DiGraph]:
+    '''
+    Generate all subgraphs for all organism and medium combinations and output them in a dictionary
+    '''
+    pass
+
+def get_amino_acids():
+    '''
+    How many/can all amino acids be synthesized?
+    '''
+    pass
+
+def compare_rec_based_on_organism():
+    '''
+    For the same organism, are there differences in the reconstruction based on the cultivation media?
+    '''
+    pass
+
+def compare_rec_based_on_medium():
+    '''
+    For the same medium, are there notable differences in the reconstructed pathways between species?
+    '''
+    pass
+
+def alternative_react_paths():
+    pass
+
