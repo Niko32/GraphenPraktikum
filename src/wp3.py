550065545f97d88659f601ed3f5b71d4d93376e6
GPraktikumaccount <gpraktikum@prak05.bioinf.uni-leipzig.de>
2023-02-22 14:22:22 +0100

    bfs completed and rebuild_molecule_edges

--- src/wp3.py
+++ src/wp3.py
@@ -40,11 +40,49 @@ def bfs_from_molecule(G: nx.Graph, molecule: str) -> nx.Graph:
     breadth first search starting at the given molecule 
     '''
     compounds = nx.get_node_attributes(G, "compound_name")
-    start_nodes = [node for node, c in compounds.items() if c == molecule]
+    compounds_reverse = {}
+    for node in compounds:
+        c = compounds[node]
+        if c in compounds_reverse:
+            compounds_reverse[c].append(node)
+        else:
+            compounds_reverse[c] = [node]
+
+    start_nodes = compounds_reverse[molecule]
 
     H = _search_edges(G, start_nodes)
 
-    return H
+    remove_nodes = []
+    for comp in compounds_reverse:
+        reached_nodes = []
+        for node in compounds_reverse[comp]:
+            if node in H.nodes:
+                reached_nodes.append(node)
+        if not len(reached_nodes) == len(compounds_reverse[comp]):
+            remove_nodes = remove_nodes + reached_nodes
+
+    H2 = H.subgraph(list(set(H.nodes)-set(remove_nodes)))
+
+    return H2
+
+
+def rebuild_molecule_edges(G_full: nx.Graph, G_sub: nx.Graph) -> nx.Graph:
+    '''
+    Adds the molecule edges (NO_TRANSITION) again to the Graph G_sub
+    '''
+    add_edges = []
+    for node in G_sub.nodes:
+        edges_full = set(G_full.edges(node))
+        edges_sub = set(G_sub.edges(node))
+        for edge in edges_full - edges_sub:
+            a, b = edge
+            if a in G_sub.nodes and b in G_sub.nodes:
+                add_edges.append((a, b))
+
+    G_out = G_sub.copy()
+    G_out.add_edges_from(add_edges)
+
+    return G_out
 
 
 if __name__ == "__main__":
@@ -60,10 +98,13 @@ if __name__ == "__main__":
     print(len(G_without_CO2.edges))
 
     G_bfs_with_CO2 = bfs_from_molecule(G_without_trans, "D-glucose")
+    G_bfs_without_CO2 = bfs_from_molecule(G_without_CO2, "D-glucose")
+
+    G_bfs_without_CO2_withMol = rebuild_molecule_edges(G, G_bfs_without_CO2)
 
 
     #third_largest_cc = G.subgraph(components[2])
-    draw_graph(G_bfs_with_CO2)
+    draw_graph(G_bfs_without_CO2_withMol)
     
     # Goals:
     # for every species medium combination
